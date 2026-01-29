20b584bcb3e8c0b5822a52e913702f289e92fef9
Wilson-Morales <92940892+Wilson-Morales@users.noreply.github.com>
2026-01-29 12:08:26 -0500

    chore: Agregar nueva columna para provincias

--- actividadEx/aplicacion/models.py
+++ actividadEx/aplicacion/models.py
@@ -8,6 +8,7 @@ class Clinica (models.Model):
     tipo =   models.CharField(max_length=20)
     direccion = models.CharField(max_length=40)
     ciudad = models.CharField(max_length=30)
+    provincia = models.CharField(max_length=30)
        
     def __str__(self):
         return "%s %s %s %s" % (self.nombre, self.tipo, self.direccion, self.ciudad)
