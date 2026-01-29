c8363e946f67bcff28a9d926bb524dab8e2ab9b8
Alexander2Morales <alexander.morales@fisagrp.com>
2026-01-29 12:28:50 -0500

    chore: add column for codigo postal

--- actividadEx/aplicacion/models.py
+++ actividadEx/aplicacion/models.py
@@ -8,6 +8,7 @@ class Clinica (models.Model):
     tipo =   models.CharField(max_length=20)
     direccion = models.CharField(max_length=40)
     ciudad = models.CharField(max_length=30)
+    cod_postal = models.IntegerField()
     parroquia = models.CharField(mas_length=30)
     provincia = models.CharField(max_length=30)
        
